49549f1bb7d0853a3890e242120ecaef0563a52c
franksharpe <145439826+franksharpe@users.noreply.github.com>
2024-03-06 21:12:40 +0000

    3rd

--- # task 1.py
+++ # task 1.py
@@ -1,3 +1,4 @@
+'''
 #task 1 
 a = int(input("Input first number: "))
 print("")
@@ -21,3 +22,31 @@ elif operation == "**":
     print("The exponentiation result is:", result)
 else:
     print("Invalid operation selected")
+
+# task 2 
+
+first = str(input("what is your first name: "))
+second = str(input("what is your second name: "))
+
+print ("Hello "+ first + " " + second )
+
+length = len(first + second)
+print("Length of name: " + str(length))
+
+reverse = input("would u like to see it backwards: (y/n) ")
+if reverse == "y":
+    print (first[::-1])
+    print (second[::-1])
+else:
+    print("loser")
+'''
+#task 3
+
+num = int(input("input a number"))
+if num < 0:
+    print("negtive")
+elif num > 1:
+    print("postive")
+elif num == 0 :
+    print("0")
+    
