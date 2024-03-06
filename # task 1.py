385cda1bf3f057b9f4f3fa3b4229b2e7a826e87b
franksharpe <145439826+franksharpe@users.noreply.github.com>
2024-03-06 20:30:41 +0000

    1

--- # task 1.py
+++ # task 1.py
@@ -1,16 +1,23 @@
 #task 1 
-a = int(input("input 1 number: "))
+a = int(input("Input first number: "))
 print("")
 print(str(a) + " is your first number")
-b = int(input("input 1 number: "))
+b = int(input("Input second number: "))
 print("")
-print(str(a) + " is your 2nd number")
+print(str(b) + " is your second number")
 
 print("What would you like to do?")
 
-c = input("Would you like to do floor division? (y/n): ")
+operation = input("Enter the operation you want to perform (floor division (//), modulus (%), exponentiation (**)): ")
 
-if c == "y":
+if operation == "//":
     result = a // b
     print("The floor division result is:", result)
-
+elif operation == "%":
+    result = a % b
+    print("The modulus result is:", result)
+elif operation == "**":
+    result = a ** b
+    print("The exponentiation result is:", result)
+else:
+    print("Invalid operation selected")
